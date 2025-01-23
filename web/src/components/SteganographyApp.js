21f010e54c7e08e9f2c93e5136f6f91245624715
IlGigante77 <tanadelgigante@outlook.com>
2025-01-23 14:19:29 +0100

    endpoint name

--- web/src/components/SteganographyApp.js
+++ web/src/components/SteganographyApp.js
@@ -48,7 +48,7 @@ const SteganographyApp = () => {
         const endpoint = isEncoding ? '/encode' : '/decode';
         
         // Use the correct API URL
-        const apiUrl = `https://stega.toki.local:71${endpoint}`;
+        const apiUrl = `http://api:71${endpoint}`;
         console.log('Calling API at:', apiUrl);
         
         try {
